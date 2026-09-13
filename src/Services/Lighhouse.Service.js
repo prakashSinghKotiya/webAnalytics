ee8f787ca32a880e9e8bb4fd857fc0bcd974c6f9
Prakash Singh Kotiya <prakashkotiya100@gmail.com>
2026-09-14 04:53:49 +0530

    initialized chrome launcher and lightout and did there baisc setup

---
+++ src/Services/Lighhouse.Service.js
@@ -0,0 +1,55 @@
+import * as chromeLauncher from "chrome-launcher"
+import lighthouse from "lighthouse"
+
+export const lighthouseReport = async(url)=>{
+
+    let chrome
+
+    try{
+
+    chrome = await chromeLauncher.launch({
+        chromeFlags : [
+           '--headless',
+        '--no-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu', // Prevents GPU-related crashes on servers
+        '--no-first-run', // Skips Chrome welcome screen
+        '--no-default-browser-check',
+        '--mute-audio', // Saves processing power
+        '--ignore-certificate-errors' // Helpful for testing staging environments
+        ]
+    })
+
+    const option ={
+        port : chrome.port, // this is how lighthouse is conneccted to chromelauncher using chromelauncher port 
+        output : "json",  
+        logLevel : "error",
+        onlyCategories: [     
+                'performance',
+                'accessibility',
+                'best-practices',
+                'seo'
+            ],
+
+    }
+
+    const result = await lighthouse(url, option)
+     if (!result) {
+            throw new Error('Lighthouse failed to generate report');
+        }
+
+
+     return result.lhr;
+
+
+
+
+    }catch (error) {
+        console.error(`[Lighthouse Error] Failed to audit ${url}:`, error.message);
+        throw new Error(`Audit failed for ${url}: ${error.message}`) 
+   
+         } finally{
+        
+            if(chrome){ chrome.kill()}
+    }
+}
