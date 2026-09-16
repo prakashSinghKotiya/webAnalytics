bea4479824fa0ff2dfdfec62e39cfd1c1a69e7b7
Prakash Singh Kotiya <prakashkotiya100@gmail.com>
2026-09-16 20:34:28 +0530

    redis fix

--- Backend/src/Workers/ttfbStart.worker.js
+++ Backend/src/Workers/ttfbStart.worker.js
@@ -1,6 +1,6 @@
 import { startTtfbWorker } from "./ttfb.Worker.js";
-// import lightHouseworker from "./Lighthouse.Worker.js"
-// import worker from "./uptime.Worker.js"
+import lightHouseworker from "./Lighthouse.Worker.js"
+import worker from "./uptime.Worker.js"
 
 const regions = [
     "india",
