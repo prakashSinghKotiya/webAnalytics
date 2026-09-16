bb9369805498454b0a66b7a8d5ef5fdd850da4e2
Prakash Singh Kotiya <prakashkotiya100@gmail.com>
2026-09-16 17:00:27 +0530

    env setup

--- Backend/src/config/redis.js
+++ Backend/src/config/redis.js
@@ -1,6 +1,10 @@
 const connection = {
   host: process.env.REDIS_HOST || "localhost",
-  port: process.env.REDIS_PORT || 6379,
+  port: Number(process.env.REDIS_PORT || 6379),
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
 };
+
+console.log("redis connected ", connection)
 
 export default connection;
