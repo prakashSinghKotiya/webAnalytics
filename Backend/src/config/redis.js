1b8274c53bb521d8fd6964b199ce4c774ddbeb59
Prakash Singh Kotiya <prakashkotiya100@gmail.com>
2026-09-16 19:36:13 +0530

    ruuning worker in main server for demo

--- Backend/src/config/redis.js
+++ Backend/src/config/redis.js
@@ -1,6 +1,6 @@
 const connection = {
-  host: process.env.REDIS_HOST || "localhost",
-  port: Number(process.env.REDIS_PORT || 6379),
+  host: process.env.REDIS_HOST ,
+  port: Number(process.env.REDIS_PORT ),
   username: process.env.REDIS_USERNAME,
   password: process.env.REDIS_PASSWORD,
 };
